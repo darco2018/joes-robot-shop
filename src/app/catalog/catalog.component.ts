66d325568d757d8ca7a489218f2f819fc2a21b4d
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-11 12:44:47 +0100

    after stashing

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+
+  product: IProduct;
+
+  constructor(){
+    
+  }
 
 }
