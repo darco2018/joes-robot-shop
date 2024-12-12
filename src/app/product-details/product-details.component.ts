5a4537fe342cbdbb9e77df6c9a4d2fe8c0e14bc2
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-12 11:13:33 +0100

    app works; empty product-details files added

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../catalog/product.model';
+
+@Component({
+  selector: 'bot-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent {}
